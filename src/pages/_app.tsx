21af22a1895f4cefc33ad6af934cf9c4d5b8408b
nto300002 <samonkntd@gmail.com>
2023-04-07 22:50:14 +0900

    portfolio

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,8 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const redirectUri = `${process.env["NEXT_PUBLIC_BASE_URL"]}/login`;
+  return (
+      <Component {...pageProps} />
+  )
 }
